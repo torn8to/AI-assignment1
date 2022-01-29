92c641d62b5a6ce1a84b9006328ab9963dbc2312
Nathan Rogers <nathanroger314@gmail.com>
2022-01-29 15:48:28 -0500

    added helper methods for boundry detection added an already searched method header

--- controller.py
+++ controller.py
@@ -14,13 +14,19 @@ class heuristic(Enum):
     better_than_sum = 'bet'
     bet_x_three = 'bx3'
 
+
+
+
 class A_star():
     def __init__(self,map,herustic):
         self.map = map
         self.heuristic = herustic
+        self.iteration = 0
 
 
-    def calculate_heuristic(self, current_pos):
+
+
+    def calculate_heuristic(self, current_pos)->int:
         better_than_sum = 0
         print(current_pos)
         comp = abs(current_pos-self.map.goal)
@@ -32,8 +38,19 @@ class A_star():
             return max(comp[0],comp[1])
         elif self.heuristic == heuristic.SUM:
             return comp[0] + comp[1]
-        elif self.heuristic == heuristic.better_than_sum
-            return  better_than_sum #todo:find a heuristic better than sum
+        elif self.heuristic == heuristic.better_than_sum:
+            return better_than_sum #todo:find a heuristic better than sum
         elif self.heuristic  == heuristic.bet_x_three:
-            return better_than_sum* 3
+            return better_than_sum * 3
 
+    def bouandry_detection(self,coords:tuple)-> bool:
+        x = coords[0]
+        y = coords[1]
+        if y<len(map) or y >= 0 or x<len(map[0]) or x >= 0:
+            return True
+        return False
+
+    def already_searched(self):
+
+    def find_path(self):
+        checked = []
