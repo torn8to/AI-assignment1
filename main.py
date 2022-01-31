09e80b3d78f940d9ce273b57b1d6a7d4bde0eade
stingraypcarter <stingraypcarter@gmail.com>
2022-01-31 18:18:02 -0500

    time and memory printouts for our max sizee
    runs in about 31s average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import psutil, os
+from datetime import datetime
 from map_generation import map_generator,Map
 from stokleyAstar import PaFinder,heuristic
 sys.setrecursionlimit(5000)
@@ -7,9 +8,10 @@ process = psutil.Process(os.getpid())
 
 if __name__ =='__main__':
     #tskrd the current memory state of the machine to compare against the ending state
-
+    initial_mem = process.memory_info().rss
+    start = datetime.now()
     if len(sys.argv) == 1:
-        map = map_generator.generate_random_map(rows=12,cols=12)
+        map = map_generator.generate_random_map(rows=4000,cols=4000)
         map_generator.map_to_file(map)
 
     else:
@@ -18,9 +20,10 @@ if __name__ =='__main__':
         file = open(file_path)
         map = map_generator.file_to_map(file)
         (print(map.map))
-        finder = PaFinder(map.map,heuristic=heuristic.bet_x_three)
+        finder = PaFinder(map.map,heuristic=heuristic.MIN)
         finder.create_frontier()
         finder.iterator()
 
 
-    print(process.memory_info().rss)
+    print((process.memory_info().rss- initial_mem)/((1024)**2))
+    print(datetime.now()-start)
