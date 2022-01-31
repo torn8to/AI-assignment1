36fbaceee1975511ed7c4024414d619a81812320
Nathan Rogers <nathanroger314@gmail.com>
2022-01-31 00:15:26 -0500

    Auto stash before merge of "pruning_frontier" and "origin/pruning_frontier"

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ if __name__ =='__main__':
     #tskrd the current memory state of the machine to compare against the ending state
 
     if len(sys.argv) == 1:
-        map = map_generator.generate_random_map(rows=12,cols=12)
+        map = map_generator.generate_random_map(rows=20,cols=20)
         map_generator.map_to_file(map)
 
     else:
-
+        init_mem = process.memory_info().rss
         file_path = sys.argv[1]
         file = open(file_path)
         map = map_generator.file_to_map(file)
         (print(map.map))
-        finder = PaFinder(map.map,heuristic=heuristic.bet_x_three)
+        finder = PaFinder(map.map,heuristic=heuristic.ZERO)
         finder.create_frontier()
         finder.iterator()
+        end_mem = process.memory_info().rss
+        print((end_mem-init_mem)/(1024**3))
 
-
-    print(process.memory_info().rss)
