deb7ea50c8d892547afb3b1c230a67693b4eec29
scvoltmer <stokleyv@gmail.com>
2022-02-01 17:53:17 -0500

    New stuff to try and facilitate a restart at a certain level of opening

--- main.py
+++ main.py
@@ -2,32 +2,26 @@ import sys
 import psutil, os
 from datetime import datetime
 from map_generation import map_generator,Map
-from stokleyAstar import PaFinder,heuristic
+from stokleyAstar import PaFinder#,# heuristic
 sys.setrecursionlimit(5000)
 process = psutil.Process(os.getpid())
 
 
-if __name__ =='__main__':
     #tskrd the current memory state of the machine to compare against the ending state
 
-    initial_mem = process.memory_info().rss
-    start = datetime.now()
+initial_mem = process.memory_info().rss
+start = datetime.now()
 
-    if len(sys.argv) == 1:
-        map = map_generator.generate_random_map(rows=4000,cols=4000)
-        map_generator.map_to_file(map)
 
-    else:
-        initial_mem = process.memory_info().rss
-        start = datetime.now()
+map = map_generator.generate_random_map(rows=50, cols=50)
+map_generator.map_to_file(map)
 
-        file_path = sys.argv[1]
-        file = open(file_path)
-        map = map_generator.file_to_map(file)
-        (print(map.map))
-        finder = PaFinder(map.map,heuristic=heuristic.zero)
-        finder.create_frontier()
-        finder.iterator()
+initial_mem = process.memory_info().rss
+start = datetime.now()
+# heuristic=heuristic.ZERO
+(print(map.map))
+finder = PaFinder(map.map)
+finder.iterator()
 
-    print('memory used: ' + str(round((process.memory_info().rss- initial_mem)/((1024)**2))) + ' mb')
-    print('time elapsed: ' + str(datetime.now()-start))
+print('memory used: ' + str(round((process.memory_info().rss- initial_mem)/((1024)**2))) + ' mb')
+print('time elapsed: ' + str(datetime.now()-start))
