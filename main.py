c174a46b372d10abe901c931d486ee46f46a6af6
stingraypcarter <stingraypcarter@gmail.com>
2022-02-02 14:37:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,26 @@ else:
     start = datetime.now()
 
     file_path = sys.argv[1]
+    chosen_h = sys.arv[2]
+    cur_h = heuristic.ZERO
+    if chosen_h = 2:
+        cur_h = heuristic.MIN
+    elif chosen_h = 3:
+        cur_h = heuristic.MAX
+    elif chosen_h = 4:
+        cur_h = heuristic.SUM
+    elif chosen_h = 5:
+        cur_h = heuristic.better_than_sum
+    elif chosen_h = 6:
+        cur_h = heuristic.bet_x_three
+
     file = open(file_path)
     map = map_generator.file_to_map(file)
 
     initial_mem = process.memory_info().rss
     start = datetime.now()
 
-    finder = PaFinder(map.map,heuristic.bet_x_three)
+    finder = PaFinder(map.map,cur_h)
     finder.iterator()
 
     print('memory used: ' + str(round((process.memory_info().rss- initial_mem)/((1024)**2))) + ' mb')
